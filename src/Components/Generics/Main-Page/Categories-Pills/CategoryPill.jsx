0629621c7fc9942dbff86916242662e574f19d19
RaulCamposEH <raul.hernandezcps@uanl.edu.mx>
2023-05-05 15:16:46 -0600

    Update CategoryPill.jsx

--- src/Components/Generics/Main-Page/Categories-Pills/CategoryPill.jsx
+++ src/Components/Generics/Main-Page/Categories-Pills/CategoryPill.jsx
@@ -1,13 +1,14 @@
 import "./CategoryPill.css"
+import { Link , Navigate } from "react-router-dom"
 
 function CategoryPill({ CategoryNumber, CategoryName, ContentCount }) {
   // const { CategoryNumber, CategoryName, ContentCount } = props
   return (
     <div className="top-cat-pill">
-      <a href={`/search/posts/${CategoryName}`}>
+      <Link to={`/search/posts/${CategoryName}`}>
         <h1>#{CategoryNumber} {CategoryName}</h1>
         <p>{ContentCount} publicaciones</p>
-      </a>
+      </Link>
     </div>
   )
 }
