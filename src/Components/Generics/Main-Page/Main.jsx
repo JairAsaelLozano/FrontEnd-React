3a999df8e76a8aad713c23130570a0b1e0f9ea32
JairAsaelLozano <74801997+JairAsaelLozano@users.noreply.github.com>
2023-05-05 16:29:20 -0600

    A

--- src/Components/Generics/Main-Page/Main.jsx
+++ src/Components/Generics/Main-Page/Main.jsx
@@ -190,7 +190,7 @@ function Main() {
         </section>
         <section className="acrylback right-section">
           <div className="top-content">
-            <img src={data.Image.secure_url} alt="imagen perfil" />
+            <img src="" alt="imagen perfil" />
             <h1>{data.UserName}</h1>
           </div>
           <div className="fill-content">
