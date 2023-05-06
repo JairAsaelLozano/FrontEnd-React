c7129a57407fdccd3ff8fd863469d075a4a17589
RaulCamposEH <raul.hernandezcps@uanl.edu.mx>
2023-05-05 23:36:40 -0600

    Update Post.jsx

--- src/Components/Generics/Post/Post.jsx
+++ src/Components/Generics/Post/Post.jsx
@@ -73,7 +73,7 @@ function Post({ isLocalUser, fndelete, srcPost, id_post}) {
           {
             isLocalUser == false ? <button onClick={openEditModalfn} className="btn-post" id="btn-post-edit"></button> : null
           }
-          <button onClick={view} className="btn-post" id="btn-like-post"></button>
+          <button onClick={view} className="btn-post" id="btn-view-post"></button>
           <button className="btn-post" id="btn-like-post"></button>
           <button onClick={openListModalfn} className="btn-post" id="btn-save-post"></button>
           <button className="btn-post" id="btn-fav-post"></button>
